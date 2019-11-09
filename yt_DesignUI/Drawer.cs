845e36acf5dddb0cf82fc7ba09c5b1e52012ecf7
Egolds <47989977+Egolds@users.noreply.github.com>
2019-11-09 13:08:47 +0100

    + Закругление
    Новый класс для закругления

---
+++ yt_DesignUI/Drawer.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace yt_DesignUI
+{
+    public static class Drawer
+    {
+        public static GraphicsPath RoundedRectangle(Rectangle rect, float RoundSize)
+        {
+            GraphicsPath gp = new GraphicsPath();
+
+            gp.AddArc(rect.X, rect.Y, RoundSize, RoundSize, 180, 90);
+            gp.AddArc(rect.X + rect.Width - RoundSize, rect.Y, RoundSize, RoundSize, 270, 90);
+            gp.AddArc(rect.X + rect.Width - RoundSize, rect.Y + rect.Height - RoundSize, RoundSize, RoundSize, 0, 90);
+            gp.AddArc(rect.X, rect.Y + rect.Height - RoundSize, RoundSize, RoundSize, 90, 90);
+
+            gp.CloseFigure();
+
+            return gp;
+        }
+    }
+}
